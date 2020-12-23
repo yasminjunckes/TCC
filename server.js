d44e5336c8eea4bd887d3510da20750227b49cd6
EullerAron <aroneuller1903@gmail.com>
2020-12-23 11:18:03 -0300

    Tela inicial - Front completo Back semi completo

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require('express');
-const app = express(); 
-const path = require('path'); 
+const app = express();
+const path = require('path');
 
-const handlebars = require('express-handlebars'); 
+const handlebars = require('express-handlebars');
 
 //configuração do handlebars como template engine
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');  
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 //public
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, function() {
-    console.log('Servidor Rodando na porta 3000...'); 
-}); 
+app.listen(3000, function () {
+    console.log('Servidor Rodando na porta 3000...');
+});
 
-app.get('/', function(req, resp) {
+app.get('/', function (req, resp) {
     resp.sendFile(__dirname + '/html/index.html'); //renderiza o arquivo formulario no main
 });
 
-app.get('/cad', function(req, resp) {
+app.get('/cad', function (req, resp) {
     resp.render('formulario'); //renderiza o arquivo formulario no main
 });
 
@@ -35,35 +35,151 @@ mongoose.connect("mongodb://localhost/allcare", {useMongoClient: true})
       console.log("Erro na conexao " + err); 
   });
 
-  const UsuarioSchema = mongoose.Schema({
 
-    nome:{
-       type: String, 
-       require: true
-    }, 
-    senha:{
-       type: Number, 
-       require: true    
-    }, 
-    email:{
-       type: String 
+const UsuarioSchema = mongoose.Schema({
+    
+    nome: {
+        type: String,
+        require: true
+    },
+    dataNasc: {
+        type: String,
+        require: true
+    },
+    senha: {
+        type: String,
+        require: true
+    },
+    CPF: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    tipoUsuario: {
+        type: String,
+        require: true
+    },
+    genero:{
+        type: String,
+        require: true
+    },
+    celular:{
+        type: String,
+        require: true
     }
 });
 
+const CuidadorSchema = mongoose.Schema({
+
+    agenda: {
+        type: String,
+        require: true
+    },
+    valor: {
+        type: Number,
+        require: true
+    },
+    descricao: {
+        type: String,
+        require: true
+    },
+    cachorro: {
+        type: Boolean,
+        require: true
+    },
+    idoso: {
+        type: Boolean,
+        require: true
+    },
+    crianca: {
+        type: Boolean,
+        require: true
+    },
+    idCuidador: {
+        type: String,
+        require: true
+    }
+
+});
+
+const ServicoSchema = mongoose.Schema({
+
+    tipo: {
+        type: String,
+        require: true
+    },
+    dataInicio: {
+        type: String,
+        require: true
+    },
+    dataFim: {
+        type: String,
+        require: true
+    },
+    idUsuarioCuidador: {
+        type: String,
+        require: true
+    },
+    idUsuarioCliente: {
+        type: String,
+        require: true
+    }
+    
+});
+
+const EnderecoSchema = mongoose.Schema({
+
+    cep: {
+        type: String,
+        require: true
+    },
+    estado: {
+        type: String,
+        require: true
+    },
+    cidade: {
+        type: String,
+        require: true
+    },
+    rua: {
+        type: String,
+        require: true
+    },
+    numero: {
+        type: String,
+        require: true
+    },
+    logradouro: {
+        type: String,
+        require: true
+    },
+    idUsuario: {
+        type: String,
+        require: true
+    }
+});
+
+
 //Atribuindo o schema à collection
 mongoose.model('user', UsuarioSchema);
+mongoose.model('cuidador', CuidadorSchema);
+mongoose.model('saervico', ServicoSchema);
+mongoose.model('endereco', EnderecoSchema);
 
 const usuario = mongoose.model('user');
 
 
 //Inserindo dados (Create do Crud)
 new usuario({
-   nome: "Ivan JB", 
-   senha: 1234, 
-   email: "ivan.borchardt.cobol@gmail.com"
+    nome: "Ivan JB",
+    senha: 1234,
+    email: "ivan.borchardt.cobol@gmail.com"
 }).save()
- .then(function(){
-     console.log("Usuario Cadastrado com sucesso!");
- }).catch(function(err){
-     console.log("Erro do DB " + err); 
- });
+    .then(function () {
+        console.log("Usuario Cadastrado com sucesso!");
+    }).catch(function (err) {
+        console.log("Erro do DB " + err);
+    });
